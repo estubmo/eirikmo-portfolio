e598a2b7af2a49fe43933ff5561a3eb3e25e1159
Eirik Mo <eirik@mowebdev.com>
2024-03-06 15:10:11 +0100

    Update cache-control.global.ts to include webp file extension

--- server/middleware/cache-control.global.ts
+++ server/middleware/cache-control.global.ts
@@ -3,6 +3,6 @@ export default defineEventHandler((event) => {
   const year = 31536000;
   const hour = 60 * 60;
   const url = event.req.url;
-  const maxage = url.match(/(.+)\.(jpg|jpeg|gif|css|png|js|ico|svg|mjs)/) ? year : hour;
+  const maxage = url.match(/(.+)\.(jpg|jpeg|gif|webp|css|png|js|ico|svg|mjs)/) ? year : hour;
   res.setHeader("Cache-Control", `max-age=${maxage} s-maxage=${maxage}`);
 });
